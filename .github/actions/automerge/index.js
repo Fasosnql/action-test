0fe6d4f6ff1a5a7459c43f30ca278698e6f3660e
jakubroch <jakub.roch@cloudinary.com>
2020-12-04 16:37:29 +0100

    action

--- .github/actions/automerge/index.js
+++ .github/actions/automerge/index.js
@@ -1,4 +1,29 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+const token = core.getInput('token');
+const client = new github.GitHub(token);
+
+async function main() {
+  const baseBranch = github.context.payload.ref;
+  const pullsResponse = await client.pulls.list({
+    ...github.context.repo,
+    base: baseBranch,
+    state: 'open',
+  });
+  const prs = pullsResponse.data;
+
+  await Promise.all(
+    prs.map((pr) => {
+      console.log(pr);
+      /*client.pulls.updateBranch({
+        ...github.context.repo,
+        pull_number: pr.number,
+      });*/
+    }),
+  );
+}
+
+main();
+
 console.log('HELLO EVERYONE 1111111111');
